eae39999e01d07a4f47fcf3b3c209e322ead83fa
Wick <wick.linxunjie@gmail.com>
2025-02-21 13:20:43 +0800

    x

--- packages/vite/rollup.config.ts
+++ packages/vite/rollup.config.ts
@@ -67,6 +67,18 @@ const sharedNodeOptions = defineConfig({
     format: 'esm',
     externalLiveBindings: false,
     freeze: false,
+    sourcemap: true,
+    sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+      // 获取每个模块的绝对路径
+      const absoluteSourcePath = path.resolve(
+        path.dirname(sourcemapPath),
+        relativeSourcePath,
+      )
+      console.log(absoluteSourcePath)
+
+      // 返回转换后的路径
+      return absoluteSourcePath
+    },
   },
   onwarn(warning, warn) {
     if (warning.message.includes('Circular dependency')) {
@@ -320,7 +332,10 @@ const __require = __cjs_createRequire(import.meta.url);
       // inject after the last `import`
       s.appendRight(index, cjsPatch)
       console.log('patched cjs context: ' + chunk.fileName)
-      return s.toString()
+      return {
+        code: s.toString(),
+        map: s.generateMap({ hires: true }),
+      }
     },
   }
 }
